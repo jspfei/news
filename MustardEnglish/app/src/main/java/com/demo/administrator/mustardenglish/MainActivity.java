99a59e95924d670a6084983ff1fee05824b4c394
fei_jiang <421436585@qq.com>
2017-01-10 23:42:10 +0800

    sss

--- MustardEnglish/app/src/main/java/com/demo/administrator/mustardenglish/MainActivity.java
+++ MustardEnglish/app/src/main/java/com/demo/administrator/mustardenglish/MainActivity.java
@@ -1,19 +1,13 @@
 package com.demo.administrator.mustardenglish;
 
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 import com.demo.administrator.mustardenglish.activity.ExerciseActivity;
-import com.demo.administrator.mustardenglish.bean.Sentence;
-import com.demo.administrator.mustardenglish.utils.ReadExcel;
-import com.demo.administrator.mustardenglish.utils.ReadExcelUtils;
 
-import java.io.IOException;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private Context context;
@@ -23,20 +17,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         intView();
         context = this;
-        try {
-            readExcel();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        //    ReadExcelUtils.readExcel(this);
-    }
-    private void readExcel() throws IOException {
-        List<Sentence> list1 = new ReadExcel().readExcel(context,"sentence.xlsx");
-        if (list1 != null) {
-            for (Sentence student : list1) {
-                System.out.println("id. : " + student.getId() + ", cn : " + student.getCn() + ", en : " + student.getEn());
-            }
-        }
+
     }
 
     private void intView() {
--- MustardEnglish/app/src/test/java/com/demo/administrator/mustardenglish/utils/ExcelReaderTest.java
+++ MustardEnglish/app/src/test/java/com/demo/administrator/mustardenglish/utils/ExcelReaderTest.java
@@ -1,16 +1,11 @@
 package com.demo.administrator.mustardenglish.utils;
 
 import android.util.Log;
-
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
 import org.junit.Test;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.List;
-
-import static org.junit.Assert.*;
+ 
 
 /**
  * Created by Administrator on 2017/1/8.
@@ -18,17 +13,6 @@ import static org.junit.Assert.*;
 public class ExcelReaderTest {
     @Test
     public void testRead() throws FileNotFoundException, IOException {
-        Workbook wb = ExcelReader.createWb("src/main/assets/sentence.xlsx");
 
-        // 获取Workbook中Sheet个数
-        int sheetTotal = wb.getNumberOfSheets();
-        Debug.printf("工作簿中的工作表个数为：{}", sheetTotal);
-
-        // 获取Sheet
-        Sheet sheet = ExcelReader.getSheet(wb, 0);
-
-        // 遍历Sheet
-        List<Object[]> list = ExcelReader.listFromSheet(sheet);
-        Log.d("TAG",list.toString());
     }
 }
